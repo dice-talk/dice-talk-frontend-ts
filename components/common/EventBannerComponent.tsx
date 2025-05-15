774ac13f149d17d076e38e3bd27ddf3ff42fc078
6feetlife <6feetlife56@gmail.com>
2025-05-15 17:33:22 +0900

    이벤트 베너 3 to 1 제외하고 구현 완료

--- components/common/EventBannerComponent.tsx
+++ components/common/EventBannerComponent.tsx
@@ -1,6 +1,6 @@
 import { EventBanner } from "@/types/EventBanner";
 import { useEffect, useRef, useState } from "react";
-import { Dimensions, FlatList, Image, NativeScrollEvent, NativeSyntheticEvent, View } from "react-native";
+import { Dimensions, FlatList, Image, NativeScrollEvent, NativeSyntheticEvent, StyleSheet, Text, View } from "react-native";
 
 
 // 현재 기기의 화면 너비를 가져와서 배너 이미지 너비로 사용
@@ -9,6 +9,7 @@ const { height } = Dimensions.get("window");
 const EventBannerComponent = () => {
     // 이벤트 요청 데이터 관리 (배열로 관리함))
   const [eventBanner, setEventBanner] = useState<EventBanner[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
 //   useEffect(() => {
 //     // 이벤트 요청이 성공했을때 응답 데이터를 이벤트 상태관리에 셋팅
@@ -17,55 +18,103 @@ const EventBannerComponent = () => {
 //   }, []);
   useEffect(() => {
     // 목 데이터로 테스트
-    const dummyData = [
-      { id: 1, imageUrl: './assets/images/EventBanner/eventbanner_01.png', title: '배너 1' },
-      { id: 2, imageUrl: './assets/images/EventBanner/eventbanner_02.png', title: '배너 2' },
-      { id: 3, imageUrl: './assets/images/EventBanner/eventbanner_03.png', title: '배너 3' },
-    ];
+    const imageMap: Record<string, any> = {
+        banner1: require('@/assets/images/EventBanner/eventBanner_01.png'),
+        banner2: require('@/assets/images/EventBanner/eventBanner_02.png'),
+        banner3: require('@/assets/images/EventBanner/eventBanner_03.png'),
+      };
+      
+      const dummyData = [
+        { id: 1, imageUrl: imageMap.banner1, title: '배너 1' },
+        { id: 2, imageUrl: imageMap.banner2, title: '배너 2' },
+        { id: 3, imageUrl: imageMap.banner3, title: '배너 3' },
+      ];
     setEventBanner(dummyData);
   }, []);
   
-  
+
   // flatList 스크롤제어를 위한 참조 변수 (리랜더링 되어도 값이 유지된다))
   const flatListRef = useRef<FlatList<EventBanner>>(null);
 
   // flatList가 드레그 후 손을 뗏을 때 실행되는 함수
-  const onScrollEndDrag = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+  const onMomentumScrollEnd = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     // 현재 스크롤된 가로 위치값 (x좌표)을 가져옴
     const offsetX = event.nativeEvent.contentOffset.x;
-    // 마지막 인덱스의 위치를 계산 (총 너비 = (마지막 인덱스) * 화면 너비)
-    const maxIndex = Math.round(eventBanner.length - 1) * width;
+    
+    // 각 이미지의 너비를 사용하여 현재 인덱스를 계산
+    const index = Math.round(offsetX / width);
+    setCurrentIndex(index);
+    
+    // 총 길이
+    const totalItems = eventBanner.length;
 
-    // 만약 사용자가 마지막 이미지까지 스크롤한 뒤 한 번 더 오른쪽으로 넘겼다면
-    if(offsetX === maxIndex) {
-        // 첫번째 이미지(인덱스 0)로 즉시 이동
-        flatListRef.current?.scrollToIndex({ index: 0, animated: false });
+    // 만약 사용자가 마지막 이미지에서 오른쪽으로 스크롤했다면
+    if (index === totalItems) {
+      // 애니메이션을 사용하여 첫 번째 이미지로 이동
+      flatListRef.current?.scrollToOffset({ offset: 0, animated: false });
     }
-  }
+  };
   return (
-      <View>
-        <FlatList
-            ref={flatListRef}
-            // 응답 데이버에서 배열 데이터 출력
-            data={eventBanner}
-            // 각 항목의 고유키 (리액트 최적화))
-            keyExtractor={(item) => item.id.toString()}
-            // 기본은 세로 스크롤인데 가로로 하게 해줌
-            horizontal={true}
-            // 한 이미지씩 페이지 스크롤 하게 해줌
-            pagingEnabled={true}
-            // 스크롤 바 보이지 않게 해줌
-            showsHorizontalScrollIndicator={false}
-            // 각 항목(배너)을 렌더링하는 함수
-            renderItem={({ item }) => (
-                // 디바이스 화면에 맞춰 비율로 이미지 크기 조정
-              <Image source={{ uri: item.imageUrl }} style={{ width, height }} />
-            )}
-            // 스크롤 마지막에서 한번더 스크롤시 실행되는 함수 (0번 인덱스로 돌아감))
-            onMomentumScrollEnd={onScrollEndDrag}
-        />
-      </View>
+    <View style={styles.container}>
+        <View>
+            <FlatList
+                style={{ width: width, height: '65%' }} // 배너 높이 조정
+                ref={flatListRef}
+                // 응답 데이버에서 배열 데이터 출력
+                data={eventBanner}
+                // 각 항목의 고유키 (리액트 최적화))
+                keyExtractor={(item) => item.id.toString()}
+                
+                // 기본은 세로 스크롤인데 가로로 하게 해줌
+                horizontal={true}
+                // 한 이미지씩 페이지 스크롤 하게 해줌
+                pagingEnabled={true}
+                // 스크롤 바 보이지 않게 해줌
+                showsHorizontalScrollIndicator={false}
+                // 각 항목(배너)을 렌더링하는 함수
+                renderItem={({ item }) => (
+                    // 디바이스 화면에 맞춰 비율로 이미지 크기 조정
+                <Image source={ item.imageUrl} style={{ width: width, height: '65%'}} />
+                )}
+                // 스크롤 마지막에서 한번더 스크롤시 실행되는 함수 (0번 인덱스로 돌아감))
+                onMomentumScrollEnd={onMomentumScrollEnd}
+            />
+            <View style={styles.pageIndicator}>
+                <Text style={styles.pageText}>{currentIndex + 1} / {eventBanner.length}</Text>
+            </View>
+        </View>
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageContainer: {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative', // 자식 요소의 절대 위치를 위해 필요
+  },
+  pageIndicator: {
+    position: 'absolute',
+    top: height * 0.16, // 하단에 위치
+    // bottom: height * 0.1, // 하단에 위치
+    width: width * 0.18,
+    height: height * 0.02,
+    right: 10, // 우측에 위치
+    backgroundColor: 'rgba(0, 0, 0, 0.129)',
+    borderRadius: 10,
+    justifyContent: 'center', // 수직 중앙 정렬
+    alignItems: 'center', // 수평 중앙 정렬
+    flexDirection: 'row',
+    zIndex: 1, // zIndex를 사용하여 앞에 배치
+
+  },
+  pageText: {
+    color: 'rgba(0, 0, 0, 0.423)',
+    fontSize: 12,
+  },
+});
 export default EventBannerComponent;
