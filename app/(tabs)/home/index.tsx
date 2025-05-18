e6c430348b23b4709243e45331f69ffddbfdfb5c
6feetlife <6feetlife56@gmail.com>
2025-05-18 12:56:39 +0900

    isPlanned에 따른 바텀시트  분기처리

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,15 +1,90 @@
-import diceFriends from '@/assets/images/home/diceFriends.png';
-import exLove from '@/assets/images/home/exLoveTheme.png';
-import heartSignal from '@/assets/images/home/heartSignalTheme.png';
+import DiceFriends from '@/assets/images/home/diceFriends.png';
+import DiceFriendsIcon from '@/assets/images/home/diceFriendsIcon.svg';
+import ExLoveIcon from '@/assets/images/home/exLoveIcon.svg';
+import ExLove from '@/assets/images/home/exLoveTheme.png';
+import HeartSignalIcon from '@/assets/images/home/heartSignalIcon.svg';
+import HartSignal from '@/assets/images/home/heartSignalTheme.png';
 import MainBackground from '@/assets/images/home/mainBackground.svg';
+import CustomBottomSheet from '@/components/common/CustomBottomSheet';
 import ThemeCarousel from "@/components/home/ThemeCarousel";
-import { Dimensions, Image, StyleSheet, View } from 'react-native';
+import { BlurView } from 'expo-blur';
 
-
-
+import { useState } from 'react';
+import { Dimensions, Image, Modal, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 const HomeScreen = () => {
   const { width, height } = Dimensions.get("window");
+  // 바텀시트 표시 여부
+  const [isBottomSheetVisible, setBottomSheetVisible] = useState(false);
+  // 바텀시트 파라미터 설정
+  const [bottomSheetParams, setBottomSheetParams] = useState<{
+    backgroundColor: string;
+    status: string;
+    icon: React.ReactNode;
+    title: string;
+    lineColor: string;
+    description: string;
+  }>({
+    backgroundColor: '#f0f0f0',
+    status: 'THEME_PLANNED',
+    icon: null,
+    title: '',
+    lineColor: '#ccc',
+    description: '',
+  });
+
+  const handleImageClick = (num: number) => {
+    // 파라미터 숫자에 따라 바텀시트 설정
+    const params = {
+      1: {
+        backgroundColor: '#6DA0E1',
+        status: 'THEME_ON',
+        icon: <DiceFriendsIcon width={width * 0.13} height={width * 0.13} />,
+        title: '다이스프렌즈',
+        lineColor: 'white',
+        description: `다이스 프렌즈에 참여하는 플레이어는 6명 입니다.
+
+다이즈 프렌즈는 2일간 진행됩니다.
+
+24시간 후 단 한명의 플레이어에게 메세지를 보낼 수 있습니다.
+(단, 발신자의 닉네임은 표시되지 않습니다.)`,
+      },
+      2: {
+        backgroundColor: '#DEC2DB',
+        status: 'THEME_ON',
+        icon: <HeartSignalIcon width={width * 0.13} height={width * 0.13} />,
+        title: '하트시그널',
+        lineColor: '#a47bd6',
+        description: `하트 시그널하우스에 입주하는 플레이어는 6명 입니다.
+
+하트 시그널 하우스에 입주한 날부터 2일간 진행됩니다.
+
+24시간 후 단 한명의 플레이어에게 메세지를 보낼 수 있습니다.
+(단, 발신자의 닉네임은 표시되지 않습니다.)`,
+      },
+      3: {
+        backgroundColor: '#EDE2E0',
+        status: 'THEME_PLANNED',
+        icon: <ExLoveIcon width={width * 0.13} height={width * 0.13} />,
+        title: '환승연애',
+        lineColor: '#ffffff',
+        description: '2025. 04. 05일에 open 됩니다!',
+      },
+      // 만약 null이 들어 올 경우 기본 설정
+    }[num] || {
+      backgroundColor: '#ffffff',
+      status: 'THEME_PLANNED',
+      icon: null,
+      title: 'Default',
+      lineColor: '#000000',
+      description: 'Default description',
+    };
+    // 바텀시트 파라미터 설정
+    setBottomSheetParams(params);
+    // 바텀시트 표시
+    setBottomSheetVisible(true);
+  };
+
   return (
     <View style={styles.container}>
       <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 0 }}>
@@ -25,16 +100,55 @@ const HomeScreen = () => {
           }}
         />
       </View>
+      {/* 케러셀 */}
       <ThemeCarousel
         pages={[
-          { num: 1, icon: <Image source={diceFriends} style={{ width: width * 0.7, height: width * 0.7 }} /> },
-          { num: 2, icon: <Image source={exLove} style={{ width: width * 0.7, height: width * 0.7 }} /> },
-          { num: 3, icon: <Image source={heartSignal} style={{ width: width * 0.7, height: width * 0.7 }} /> },
+          { num: 1, icon: (
+            // 캐러셀 이미지 클릭 시 바텀시트 표시
+            <TouchableOpacity onPress={() => handleImageClick(1)}>
+              <Image source={DiceFriends} style={{ width: width * 0.7, height: width * 0.7 }} />
+            </TouchableOpacity>
+          ) },
+          { num: 2, icon: (
+            <TouchableOpacity onPress={() => handleImageClick(2)}>
+              <Image source={HartSignal} style={{ width: width * 0.7, height: width * 0.7 }} />
+            </TouchableOpacity>
+          ) },
+          { num: 3, icon: (
+            <TouchableOpacity onPress={() => handleImageClick(3)}>
+              <Image source={ExLove} style={{ width: width * 0.7, height: width * 0.7 }} />
+            </TouchableOpacity>
+          ) },
         ]}
         pageWidth={width * 0.75}
         gap={16}
         offset={width * 0.1}
       />
+      <Modal
+        visible={isBottomSheetVisible}
+        transparent
+        animationType="slide" // slide도 가능
+        onRequestClose={() => setBottomSheetVisible(false)} // Android 뒤로가기용
+      >
+        {isBottomSheetVisible && (
+        <View style={styles.overlay}>
+          {/* 🔹 전체 화면에 blur 효과 */}
+          <BlurView intensity={50} tint="light" style={StyleSheet.absoluteFill} />
+          {/* 🔹 바텀시트 */}
+          <View style={styles.bottomSheetWrapper}>
+            <CustomBottomSheet
+              isPlanned={bottomSheetParams.status === 'THEME_PLANNED'}
+              backgroundColor={bottomSheetParams.backgroundColor}
+              icon={bottomSheetParams.icon}
+              title={bottomSheetParams.title}
+              lineColor={bottomSheetParams.lineColor}
+              description={bottomSheetParams.description}
+              onClose={() => setBottomSheetVisible(false)}
+            />
+          </View>
+        </View>
+      )}
+      </Modal>
     </View>
   );
 };
@@ -42,7 +156,24 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: 'red',
+  },
+  bottomSheetWrapper: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 100,     // 다른 요소보다 위에 배치
+    elevation: 10,   // Android용 (zIndex 보완)
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,  // 전체 화면 덮기
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 100,
+    elevation: 10,
+    justifyContent: 'flex-end', // 바텀시트를 아래로 정렬
   },
 });
 
--- components/home/ThemeCarousel.tsx
+++ components/home/ThemeCarousel.tsx
@@ -3,44 +3,65 @@ import { FlatList, StyleSheet, View } from 'react-native';
 import Page from './ThemeCarouselPage';
 
 interface ICarousel {
-  gap: number;
-  offset: number;
-  pages: any[];
-  pageWidth: number;
+  gap: number; // 각 페이지 사이의 간격
+  offset: number; // 첫 페이지 시작 위치에 대한 여백
+  pages: any[]; // 렌더링할 페이지 리스트
+  pageWidth: number; // 각 페이지의 너비
 }
 
+// 캐러셀 컴포넌트 정의
 export default function Carousel({ pages, pageWidth, gap, offset }: ICarousel) {
+  // 현재 페이지 index 상태 저장
   const [page, setPage] = useState(0);
 
+  // FlatList의 각 아이템을 어떻게 렌더링할지 정의
   function renderItem({ item }: any) {
     return (
+      // 각 페이지에 전달할 스타일
       <Page item={item} style={{ width: pageWidth, marginHorizontal: gap / 2 }} />
     );
   }
 
+  // 스크롤 시 현재 페이지 index 계산
   const onScroll = (e: any) => {
+    
     const newPage = Math.round(
+      // 현재 스크롤 위치 / (페이지 + 간격) = 현재 페이지 번호)
       e.nativeEvent.contentOffset.x / (pageWidth + gap)
     );
+    // 현재 페이지 상태 업데이트
     setPage(newPage);
   };
 
   return (
     <View style={styles.container}>
       <FlatList
+        // ios 자동 보정 기능 비활성화
         automaticallyAdjustContentInsets={false}
+        // 첫 페이지 시작 위치에 대한 여백 추가
         contentContainerStyle={{
+          // 좌우 여백 추가 (offest 보정)
           paddingHorizontal: offset + gap / 2,
         }}
+        // 렌더링할 페이지 리스트
         data={pages}
+        // 스크롤 속도 설정
         decelerationRate="fast"
+        // 가로 스크롤
         horizontal
+        // 고유키 생성
         keyExtractor={(item: any, index: number) => `page__${index}`}
+        // 스크롤 이벤트 처리
         onScroll={onScroll}
+        // 페이지 스크롤 활성화
         pagingEnabled
+        // 스크롤 아이템 렌더링
         renderItem={renderItem}
+        // 페이지 스크롤 간격 설정
         snapToInterval={pageWidth + gap}
+        // 스크롤 정렬 방식
         snapToAlignment="start"
+        // 수평 스크롤 인디케이터 숨김
         showsHorizontalScrollIndicator={false}
       />
       <View style={styles.indicatorWrapper}>
