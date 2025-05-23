f8b16afc43295b181344dd70c38f3b8ef6d90d4e
6feetlife <6feetlife56@gmail.com>
2025-05-23 19:22:03 +0900

    여기까지 읽었습니다

--- components/chat/ReadingTag.tsx
+++ components/chat/ReadingTag.tsx
@@ -1,10 +1,19 @@
-import { Text, View } from "react-native";
-import { StyleSheet } from "react-native";
+import React from 'react';
+import {
+    Dimensions,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
 
-const ReadingTag = () => {
+const { width } = Dimensions.get("window");
+
+const ReadingTag: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>ReadingTag</Text>
+      <View style={styles.tag}>
+        <Text style={styles.text}>여기까지 읽으셨습니다</Text>
+      </View>
     </View>
   );
 };
@@ -13,6 +22,25 @@ export default ReadingTag;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "red",
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  tag: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#FFC0CB',
+    borderRadius: 25,
+    paddingVertical: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width * 0.4,
+  },
+  text: {
+    color: '#FF8FAB',
+    fontSize: 12,
+    fontWeight: '400',
+    textAlign: 'center',
+    letterSpacing: -0.5,
   },
 });
