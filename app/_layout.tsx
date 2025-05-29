cbff1bcc53260d0d7572347f02ced786a5db2fdc
YeoSeungA <silo1116@naver.com>
2025-05-29 09:33:07 +0900

    loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(onBoard)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="Loading" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
