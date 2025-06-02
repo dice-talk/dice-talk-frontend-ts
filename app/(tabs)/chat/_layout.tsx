7cb7bbafa621668c5c28ad248acff13fd7718d5f
YeoSeungA <silo1116@naver.com>
2025-06-02 13:51:21 +0900

    신고 페이지 id별 분리

--- app/(tabs)/chat/_layout.tsx
+++ app/(tabs)/chat/_layout.tsx
@@ -5,6 +5,7 @@ export default function ChatLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="ChatRoom" />
+        <Stack.Screen name="report/[id]" />
       </Stack>
     );
 }
